2b3a235b61ae5e20e20d6840847892f01b33425c
rafalkac02 <69152315+rafalkac02@users.noreply.github.com>
2025-03-29 10:31:37 +0000

    Apply .gitignore rules

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.get("/about", (req, res) => {
     res.send("Welcome to the About Page!");
 });
 
+// define /about route
+app.get("/contact", (req, res) => {
+    res.send("Contact us at contact@example.com!");
+});
+
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
